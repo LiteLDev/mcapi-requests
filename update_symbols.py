09c707bac59f2b727b3fee0530dd4e13d7dc59a4
Zijian <futrime+github.com@outlook.com>
2025-05-02 08:28:24 +0000

    Directly process symbols with python script

--- update_symbols.py
+++ update_symbols.py
@@ -1,7 +1,9 @@
-"""Tidy symbols."""
+"""Update symbols."""
 
+import json
 from typing import Iterable, List
 
+ISSUE_PARSER_OUTPUT_FILE_PATH = "~/issue-parser-result.json"
 SYMBOLS_FILE_PATH = "symbols.txt"
 
 
@@ -27,8 +29,15 @@ def tidy_items(items: Iterable[str]) -> List[str]:
 def main() -> None:
     """Main function."""
 
+    with open(ISSUE_PARSER_OUTPUT_FILE_PATH, "r", encoding="utf-8") as f:
+        issue_parser_result = json.load(f)
+
+    parsed_symbols = str(issue_parser_result.symbols).splitlines()
+
     with open(SYMBOLS_FILE_PATH, "r", encoding="utf-8") as f:
         symbols = f.read().splitlines()
+
+    symbols.extend(parsed_symbols)
 
     symbols = tidy_items(symbols)
 
