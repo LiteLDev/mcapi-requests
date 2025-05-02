f18263e525cc569758830e195b93824079867fe2
Zijian <futrime+github.com@outlook.com>
2025-05-02 08:32:59 +0000

    Try to fix echo problem

--- tidy_symbols.py
+++ tidy_symbols.py
@@ -1,9 +1,7 @@
-"""Update symbols."""
+"""Tidy symbols."""
 
-import json
 from typing import Iterable, List
 
-ISSUE_PARSER_OUTPUT_FILE_PATH = "~/issue-parser-result.json"
 SYMBOLS_FILE_PATH = "symbols.txt"
 
 
@@ -29,15 +27,8 @@ def tidy_items(items: Iterable[str]) -> List[str]:
 def main() -> None:
     """Main function."""
 
-    with open(ISSUE_PARSER_OUTPUT_FILE_PATH, "r", encoding="utf-8") as f:
-        issue_parser_result = json.load(f)
-
-    parsed_symbols = str(issue_parser_result.symbols).splitlines()
-
     with open(SYMBOLS_FILE_PATH, "r", encoding="utf-8") as f:
         symbols = f.read().splitlines()
-
-    symbols.extend(parsed_symbols)
 
     symbols = tidy_items(symbols)
 
