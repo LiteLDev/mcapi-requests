00ff1ec688951a5bac65e5cd64e9ee57b91fbb74
Zijian <futrime+github.com@outlook.com>
2025-05-02 08:44:42 +0000

    Try to fix

--- update_symbols.py
+++ update_symbols.py
@@ -1,7 +1,9 @@
 """Tidy symbols."""
 
+import json
 from typing import Iterable, List
 
+ISSUE_PARSER_RESULT_FILE_PATH = "issue-parser-result.json"
 SYMBOLS_FILE_PATH = "symbols.txt"
 
 
@@ -27,8 +29,17 @@ def tidy_items(items: Iterable[str]) -> List[str]:
 def main() -> None:
     """Main function."""
 
+    with open(ISSUE_PARSER_RESULT_FILE_PATH, "r", encoding="utf-8") as f:
+        issue_parser_result = json.load(f)
+
+    new_symbols_text = str(issue_parser_result.symbols)
+
+    new_symbols = new_symbols_text.split()
+
     with open(SYMBOLS_FILE_PATH, "r", encoding="utf-8") as f:
-        symbols = f.read().splitlines()
+        symbols = f.read().split()
+
+    symbols.extend(new_symbols)
 
     symbols = tidy_items(symbols)
 
